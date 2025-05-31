e92d83d87e3a58696b5b6cf3a8279e573380feb9
Thallentos <thallescmarques@gmail.com>
2025-05-31 11:58:42 -0300

    finalização do módulo "Inicializando projetos Node"

---
+++ Módulos/1- Fundamentos Node.js/1- criando w observando um projeto node/src/criarEObservar.js
@@ -0,0 +1,17 @@
+console.log("Olá Thallentos!");
+
+/**
+ * Como criar um projeto Node.js
+ * 
+ * 1- abrir o terminal
+ * 2- digitar o código "npm init -y"
+ */
+
+/**
+ * Como observar um projeto Node.js
+ * 
+ * 1- abrir o terminal
+ * 2- digitar o código "node --watch index.js"
+ * Isso serve para, toda vez que eu alterar o index.js, ele vai executar o index.js
+ * Aperte Control + C para parar
+ */
