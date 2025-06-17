58f01038d811ad572e737661609147645f14d84e
Thallentos <thallescmarques@gmail.com>
2025-06-17 16:31:38 -0300

    mostrando outras formas de importar

--- Módulos/2- Node Modules/ESM Modules/src/index.js
+++ Módulos/2- Node Modules/ESM Modules/src/index.js
@@ -1,8 +1,19 @@
-//Faço do jeito abaixo caso eu queira exportar mais de uma coisa
-import * as database from "./utils/database.js";
+//1- Faço do jeito abaixo caso eu queira exportar mais de uma coisa
+// import * as database from "./utils/database.js";
 
-//Faço do jeito abaixo caso eu queira importar só uma coisa
+// Chamo as funções desse jeito caso eu as chame do jeito acime
+// database.connectToDatabase("thallentos-database");
+// database.disconnectToDatabase("thallentos-database");
+
+//2- Faço do jeito abaixo caso eu queira importar só uma coisa
 // import connectToDatabase from "./utils/database.js";
 
-database.connectToDatabase("thallentos-database");
-database.disconnectToDatabase("thallentos-database");
+// Chamo as funções desse jeito caso eu as chame do jeito acime
+// connectToDatabase("thallentos-database");
+
+//3- Faço do jeito abaixo caso eu queira exportar mais de uma coisa
+// import {connectToDatabase, disconnectToDatabase} from "./utils/database.js";
+
+// Chamo as funções desse jeito caso eu as chame do jeito acime
+// connectToDatabase("thallentos-database");
+// disconnectToDatabase("thallentos-database");
