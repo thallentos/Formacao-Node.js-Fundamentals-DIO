4a5a2ad8b0377618c1c43bcf770e7f27bd42f1e2
Thallentos <thallescmarques@gmail.com>
2025-06-24 22:19:30 -0300

    finalização do módulo

---
+++ Módulos/3- Gerenciando Pacotes no Node.js com NPM/1- Usando NPMs/Chalk e Terminal Log/src/index.js
@@ -0,0 +1,6 @@
+import chalk from 'chalk';
+import logSymbols from 'log-symbols';
+
+console.log(chalk.cyan("Thallentos na área😎"));
+console.log(logSymbols.success, "Servidor iniciado com sucesso!");
+console.log(logSymbols.error, chalk.red.italic("Servidor com problema"));
